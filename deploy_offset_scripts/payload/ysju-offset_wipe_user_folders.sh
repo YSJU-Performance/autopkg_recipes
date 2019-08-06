ebcc431eaa525eac3817b0f2f27f24e6e05cc199
Andy Farthing <contact@andyfarthing.com>
2019-08-06 22:15:39 +0100

    Initial Offset package recipe

---
+++ deploy_offset_scripts/payload/ysju-offset_wipe_user_folders.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Wipes the user folders on logout
+
+userFolder="Student"
+
+rm -Rf /Users/$userFolder/Applications/* 
+rm -Rf /Users/$userFolder/Desktop/* 
+rm -Rf /Users/$userFolder/Documents/* 
+rm -Rf /Users/$userFolder/Downloads/* 
+rm -Rf /Users/$userFolder/Movies/* 
+rm -Rf /Users/$userFolder/Music/* 
+rm -Rf /Users/$userFolder/Pictures/* 
+rm -Rf /Users/$userFolder/Public/*
+
+
+
+chmod 755 test.sh
+chown root:wheel test.sh
+
