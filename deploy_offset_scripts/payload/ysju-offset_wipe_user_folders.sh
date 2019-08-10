589f15530020061768e8d2d1b2ad658071202061
Andy Farthing <contact@andyfarthing.com>
2019-08-10 19:55:12 +0100

    Leave a warning message in the wiped folders

--- deploy_offset_scripts/payload/ysju-offset_wipe_user_folders.sh
+++ deploy_offset_scripts/payload/ysju-offset_wipe_user_folders.sh
@@ -12,3 +12,10 @@ rm -Rf /Users/$userFolder/Movies/*
 rm -Rf /Users/$userFolder/Music/* 
 rm -Rf /Users/$userFolder/Pictures/* 
 rm -Rf /Users/$userFolder/Public/*
+
+touch "/Users/$userFolder/Documents/__DO NOT LEAVE FILES HERE"
+touch "/Users/$userFolder/Downloads/__DO NOT LEAVE FILES HERE"
+touch "/Users/$userFolder/Movies/__DO NOT LEAVE FILES HERE"
+touch "/Users/$userFolder/Music/__DO NOT LEAVE FILES HERE"
+touch "/Users/$userFolder/Pictures/__DO NOT LEAVE FILES HERE"
+touch "/Users/$userFolder/Public/__DO NOT LEAVE FILES HERE"
